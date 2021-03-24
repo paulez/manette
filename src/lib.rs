61814be87b6286bbb1d23c9b29eb87394304d7a1
Paul Ezvan <paul@ezvan.fr>
2021-03-24 22:51:44 +0100

    Dummy cursive ui.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use std::error::Error;
+use cursive::{Cursive, CursiveExt};
+use cursive::views::TextView;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut siv = Cursive::new();
+    siv.add_layer(TextView::new("Hello World!\nPress q to quit."));
+    siv.add_global_callback('q', |s| s.quit());
+    siv.run();
     Ok(())
 }
 
