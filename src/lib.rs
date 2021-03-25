a7c1d927e3c6137fa2996341e9e6df1cf30663cb
Paul Ezvan <paul@ezvan.fr>
2021-03-25 23:05:40 +0100

    Layout.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
 use std::error::Error;
 use cursive::{Cursive, CursiveExt};
-use cursive::views::TextView;
+use cursive::views::{DummyView, LinearLayout, Panel, EditView, TextView, ResizedView};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut siv = Cursive::new();
-    siv.add_layer(TextView::new("Hello World!\nPress q to quit."));
+    siv.add_layer(
+        Panel::new(
+        LinearLayout::vertical()
+                .child(EditView::new())
+                .child(DummyView)
+                .child(ResizedView::with_full_screen(
+                    TextView::new("Command output")
+                ))
+        )
+            .title("manette")
+    );
     siv.add_global_callback('q', |s| s.quit());
     siv.run();
     Ok(())
