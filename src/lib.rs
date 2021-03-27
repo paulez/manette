489b72495729ccbcab31ee9d2f1aa117af785b9f
Paul Ezvan <paul@ezvan.fr>
2021-03-27 13:59:33 +0100

    Add logger.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,52 @@
 use std::error::Error;
 use cursive::{Cursive, CursiveExt};
 use cursive::views::{DummyView, LinearLayout, Panel, EditView, TextView, ResizedView};
+use cursive_core::view::Nameable;
+use cursive_flexi_logger_view::FlexiLoggerView;
+use flexi_logger::{Logger, LogTarget};
+
+mod command;
+
+use crate::command::run;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut siv = Cursive::new();
+    Logger::with_env_or_str("info, manette = debug")
+        .log_target(LogTarget::FileAndWriter(
+            cursive_flexi_logger_view::cursive_flexi_logger(&siv),
+        ))
+        .directory("logs")
+        .suppress_timestamp()
+        .format(flexi_logger::colored_with_thread)
+        .start()
+        .expect("failed to initialize logger!");
+
     siv.add_layer(
         Panel::new(
         LinearLayout::vertical()
-                .child(EditView::new())
+                .child(EditView::new()
+                       .on_submit(user_input)
+                       .with_name("command_input")
+                )
                 .child(DummyView)
                 .child(ResizedView::with_full_screen(
                     TextView::new("Command output")
+                        .with_name("command_output")
                 ))
+            .child(FlexiLoggerView::scrollable())
         )
             .title("manette")
     );
     siv.add_global_callback('q', |s| s.quit());
+    log::info!("test log message");
     siv.run();
     Ok(())
+}
+
+fn user_input(s: &mut Cursive, command: &str) {
+    s.call_on_name("command_output", |view: &mut TextView| {
+        view.set_content(run::run_command(command));
+    });
 }
 
 pub struct Config {
